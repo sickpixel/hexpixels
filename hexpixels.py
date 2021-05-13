3893be1a582ea4f72495d2f00ad797fa2a612b25
Jupiter Bott <jupiter@boltt.org>
2021-05-12 19:17:15 -0700

    added theading

--- hexpixels.py
+++ hexpixels.py
@@ -1,7 +1,12 @@
 import board
 import neopixel
+import time
+import logging
+LOG = logging.getLogger(__name__)
 
-class HexPixels:
+from PySide2 import QtCore
+
+class HexPixels(QtCore.QObject):
 
     # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
     # NeoPixels must be connected to D10, D12, D18 or D21 to work.
@@ -14,6 +19,7 @@ class HexPixels:
         For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
 
         """
+        super(HexPixels, self).__init__()
         self.pixels = neopixel.NeoPixel(
             self.PIXEL_PIN, 
             num_pixels, 
@@ -21,6 +27,25 @@ class HexPixels:
             auto_write=False, 
             pixel_order=pixel_order
         )
+
+        self.start_color = {"R":255, "G":0, "B":0}
+        self.end_color = {"R":0, "G":0, "B":255}
+
+        self.stop_received = False
+        self.stopped = False
+
+        self.breathe_in = False
+
+        self.mutex = QtCore.QMutex()
+
+    def run(self):
+        LOG.debug('[{0}] HexPixels::run'.format(QtCore.QThread.currentThread().objectName()))
+        counter = 0
+        while not self.stop_received:
+            self.do_fade(counter)
+            counter += 1
+            time.sleep(0.1)
+        self.stopped = True
 
     def fade_single(self, color,hex_index):
         for i in range (30):
@@ -53,3 +78,54 @@ class HexPixels:
 
     def show(self):
         self.pixels.show()
+
+
+    ##########################################
+    # PATTERNS
+
+    def set_start_color(self, start_color):
+        LOG.debug('set_start_color: {0}'.format(start_color))
+        self.mutex.lock()
+        self.start_color = start_color
+        self.mutex.unlock()
+
+    def set_end_color(self, end_color):
+        LOG.debug('set_end_color: {0}'.format(end_color))
+        self.mutex.lock()
+        self.end_color = end_color
+        self.mutex.unlock()
+
+    def do_fade(self, counter):
+        if counter % 100 == 0 :
+            self.breathe_in = not self.breathe_in
+        count = (counter % 100) / 100.0
+        if self.breathe_in:
+            color1= self.get_fade_color(self.start_color, self.end_color, count)
+            color2= self.get_fade_color(self.end_color, self.start_color, count)
+        else:
+            color1= self.get_fade_color(self.end_color, self.start_color, count)
+            color2= self.get_fade_color(self.start_color, self.end_color, count)
+        self.fade_multiple(color1,[0,2,4,6,8,10,12])
+        self.fade_multiple(color2,[1,3,5,7,9,11])
+        self.show()
+
+    #     for i in range(100):
+
+    #         color= HP.get_fade_color(END_COLOR, START_COLOR, i/100.0)
+    #         ##fade_single(color,7)
+    #         HP.fade_multiple(color,[2,5,7])
+
+    #         color= HP.get_fade_color(START_COLOR, END_COLOR, i/100.0)
+    #         HP.fade_single(color,8)
+
+
+    #         HP.show()
+    #         time.sleep(0.01)
+
+
+    def shutdown(self):
+        LOG.debug('[{0}] HexPixels::stop received'.format(QtCore.QThread.currentThread().objectName()))
+        self.stop_received = True
+        while not self.stopped:
+            QtCore.QThread.msleep(100)
+        LOG.debug('[{0}] HexPixels::stopped'.format(QtCore.QThread.currentThread().objectName()))
--- pyside_hello_world.py
+++ pyside_hello_world.py
@@ -1,5 +1,5 @@
 import sys
-from PySide2 import QtWidgets
+from PySide2 import QtCore, QtWidgets
 
 # Simple test for NeoPixels on Raspberry Pi
 import time
@@ -7,41 +7,47 @@ import select
 import math
 import hexpixels
 
-START_COLOR = {"R":255, "G":0, "B":0}
-END_COLOR = {"R":0, "G":0, "B":255}
-
-HP = hexpixels.HexPixels(390, 0.7)
-
-def do_fade():
-    for i in range(100):
-
-        color= HP.get_fade_color(START_COLOR, END_COLOR, i/100.0)
-        ##fade_single(color,7)
-        HP.fade_multiple(color,[2,5,7])
-
-        color= HP.get_fade_color(END_COLOR, START_COLOR, i/100.0)
-        HP.fade_single(color,8)
+import logging
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
-        HP.show()
-        time.sleep(0.01)
-
-    for i in range(100):
-
-        color= HP.get_fade_color(END_COLOR, START_COLOR, i/100.0)
-        ##fade_single(color,7)
-        HP.fade_multiple(color,[2,5,7])
-
-        color= HP.get_fade_color(START_COLOR, END_COLOR, i/100.0)
-        HP.fade_single(color,8)
 
 
-        HP.show()
-        time.sleep(0.01)
+
+# def do_fade():
+#     for i in range(100):
+
+#         color= HP.get_fade_color(START_COLOR, END_COLOR, i/100.0)
+#         ##fade_single(color,7)
+#         HP.fade_multiple(color,[2,5,7])
+
+#         color= HP.get_fade_color(END_COLOR, START_COLOR, i/100.0)
+#         HP.fade_single(color,8)
+
+
+#         HP.show()
+#         time.sleep(0.01)
+
+#     for i in range(100):
+
+#         color= HP.get_fade_color(END_COLOR, START_COLOR, i/100.0)
+#         ##fade_single(color,7)
+#         HP.fade_multiple(color,[2,5,7])
+
+#         color= HP.get_fade_color(START_COLOR, END_COLOR, i/100.0)
+#         HP.fade_single(color,8)
+
+
+#         HP.show()
+#         time.sleep(0.01)
 
 
 
 class MainWindow(QtWidgets.QWidget):
+
+    #send_start_color = QtCore.Signal(dict)
+    #send_end_color = QtCore.Signal(dict)
 
     def __init__(self, parent=None):
         super().__init__()
@@ -53,7 +59,7 @@ class MainWindow(QtWidgets.QWidget):
         self.end_color_button.clicked.connect(self.choose_end_color)
 
         self.do_fade_button = QtWidgets.QPushButton("Do Fade")
-        self.do_fade_button.clicked.connect(do_fade)
+        # self.do_fade_button.clicked.connect(do_fade)
 
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.start_color_button)
@@ -62,21 +68,40 @@ class MainWindow(QtWidgets.QWidget):
 
         self.setLayout(self.layout)
         self.show()
+        QtCore.QThread.currentThread().setObjectName("MAIN")
+        LOG.debug('[{0}] HexPixelsUI::run'.format(QtCore.QThread.currentThread().objectName()))
+        self.hp = hexpixels.HexPixels(390, 0.5)
+
+        #self.send_start_color.connect(self.hp.set_start_color, QtCore.Qt.QueuedConnection)
+        #self.send_end_color.connect(self.hp.set_end_color, QtCore.Qt.QueuedConnection)
+        
+        self.hp_thread = QtCore.QThread()
+        self.hp_thread.setObjectName("HexPixels")
+        self.hp.moveToThread(self.hp_thread)
+        self.hp_thread.started.connect(self.hp.run, type=QtCore.Qt.QueuedConnection)
+        self.hp_thread.start()
 
     def choose_start_color(self):
         global START_COLOR
         color = QtWidgets.QColorDialog.getColor()
-        START_COLOR = {"R":color.red(), "G":color.green(), "B":color.blue()}
-        print(START_COLOR)
+        #START_COLOR = {"R":color.red(), "G":color.green(), "B":color.blue()}
+        self.hp.set_start_color({"R":color.red(), "G":color.green(), "B":color.blue()})
+        #print(START_COLOR)
         self.start_color_button.setStyleSheet("background-color: %s"%color.name())
-
 
     def choose_end_color(self):
         global END_COLOR
         color = QtWidgets.QColorDialog.getColor()
-        END_COLOR = {"R":color.red(), "G":color.green(), "B":color.blue()}
-        print(END_COLOR)
+        #END_COLOR = {"R":color.red(), "G":color.green(), "B":color.blue()}
+        self.hp.set_end_color({"R":color.red(), "G":color.green(), "B":color.blue()})
+        #print(END_COLOR)
         self.end_color_button.setStyleSheet("background-color: %s"%color.name())
+
+    def closeEvent(self, event):
+        self.hp.shutdown()
+        self.hp_thread.quit()
+        self.hp_thread.wait()
+        event.accept()
 
 app = QtWidgets.QApplication(sys.argv)
 main_window = MainWindow()
